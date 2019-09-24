a0be12b5b147340a2df0496470fcda756b8df3d0
Merry Cimakasky <mkcimakasky@gmail.com>
2019-09-24 12:43:17 -0700

    ceasar hack working

---
+++ python/cracking_codes_with_python/6_hacking_the_ceasar_cipher_with_brute-force/caesar_hack.py
@@ -0,0 +1,25 @@
+# Caesar Cypher hack as enumerated in Cracking Codes with Python. 
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+message = input('Enter your ciphertext here. ') 
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz1234567890!?.,;:()'
+
+for key in range(len(SYMBOLS)):
+  translated = ''
+# Loop through each symbol in message:
+  for symbol in message:
+      if symbol in SYMBOLS:
+          symbolIndex = SYMBOLS.find(symbol)
+          translatedIndex = symbolIndex - key
+          # Handle the wraparound:
+          if translatedIndex < 0:
+              translatedIndex = translatedIndex + len(SYMBOLS)
+          # Append the decrypted symbol:
+          translated = translated + SYMBOLS[translatedIndex]
+      else:
+          # Append the symbol without encrypting/decrypting:
+          translated = translated + symbol
+# Display every possible decryption:
+  print('Key #%s: %s' % (key, translated))     
+        
+
