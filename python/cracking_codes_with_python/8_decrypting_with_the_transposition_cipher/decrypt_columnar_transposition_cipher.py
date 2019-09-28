4db9106984f8c11c203b1fb6896fc623c370ff28
Merry Cimakasky <mkcimakasky@gmail.com>
2019-09-27 18:24:42 -0700

    finished decrypt columnar transpotion cipher .py, need to finish going through chapter 8

---
+++ python/cracking_codes_with_python/8_decrypting_with_the_transposition_cipher/decrypt_columnar_transposition_cipher.py
@@ -0,0 +1,38 @@
+# Columnar Transposition Ciper Decryption inspired by chapter 8 of Cracking Codes with Python 
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import math
+
+ciphertext = input('What is the text you would like to decrypt? ')
+key = int(input('What is the key you would like to use? '))
+
+def decrypt_message(key, ciphertext): 
+
+  num_columns = int(math.ceil(len(ciphertext)/float(key)))
+
+  #necessary?
+  num_rows = key
+
+  extra_boxes = (num_columns * key) - len(ciphertext) 
+
+  plain_text = [''] * key
+
+  cur_column = 0
+  cur_row = 0
+
+  for char in ciphertext: 
+
+    plain_text[cur_column] += char
+    cur_column += 1
+
+    if (cur_column == num_columns) or (cur_column == num_columns - 1 and cur_row >= key - extra_boxes): 
+
+      cur_column = 0
+      cur_row += 1
+
+  return_value = ''.join(plain_text)  
+  print(return_value)
+  return return_value     
+
+if __name__ == '__main__': 
+  decrypt_message(key, ciphertext)
