0c1fee348bd7e28b24385cb5c80b88efc55a9bda
Merry Cimakasky <mkcimakasky@gmail.com>
2019-10-01 14:04:17 -0700

    test written, some questions on if the import statements will work

---
+++ python/cracking_codes_with_python/h_programming_a_program_to_test_your_program/columnar_transposition_test.py
@@ -0,0 +1,32 @@
+# Automated Testing for a Columnar Transpotion Cypher encoder and decoder per Cracking Codes with Python
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import random, sys
+from ..f_encrypting_with_the_transposition_cipher import columnar_cipher
+
+from ..g_decrypting_with_the_transposition_cipher.decrypt_columnar_transposition_cipher import decrypt_message
+
+def main(): 
+  random.seed(802) 
+
+  for i in range(20): 
+    message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * random.randint(4, 40) 
+
+    message = list(message) 
+
+    random.shuffle(message) 
+    message = ''.join(message)
+    print('Test #%s: "%s..."'%(i+1, message[:50]))
+
+    for key in range(1, int(len(message)/2)):
+      encrypted = colmunar_cipher.colmunar_cipher(key, message) 
+      decrypted = decrypt_message.decrypt_message(key, encrypted)
+
+      if message != decrypted: 
+        print('Mismatch with key %s and message %s.' % (key, message))
+        print('Decrypted as: ' + decrypted) 
+        sys.exit()
+  print('Transposition cipher test passed.')  
+
+  if __name__ == '__main__': 
+    main()    
